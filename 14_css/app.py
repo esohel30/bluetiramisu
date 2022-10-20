baa557f3c04a53b5632081f5195f895613918732
esohel30 <esohel30@stuy.edu>
2022-10-19 23:01:43 -0400

    updated code

--- 14_css/app.py
+++ 14_css/app.py
@@ -1,20 +1,13 @@
-# Q0: What will happen if you remove render_template from this line? (log your prediction before you pull the trigger...)
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 
-
-# Q1: Can all of your teammates confidently predict the URL to use to load this page?
-@app.route("/my_foist_template")
-def test_tmplt():
-    # Q2: What is the significance of each argument? Simplest, most concise answer best.
-    return render_template('index.html', foo="fooooo", collection=coll)
-
-
-@app.route("/foo")
-def test():
-    return render_template("index.html")
-
+@app.route("/")
+def display():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.debug = True
---
+++ 14_css/readme.md
@@ -0,0 +1,5 @@
+DISCOS:
+-What exactly is inspect element anyways bro?
+
+QCCS:
+- What is another way we can link our flask terminal to our html code to run it on a live server. 
